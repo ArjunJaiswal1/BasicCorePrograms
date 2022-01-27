6fb291c15cc4e52f28e11ce9d9cd41517d80b8e6
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-01-27 23:18:04 +0530

    [Arjun] Add ; Added Elapsed

--- BasicCoreProgram/FlipCoin.cs
+++ BasicCoreProgram/FlipCoin.cs
@@ -105,8 +105,20 @@ namespace BasicCoreProgram
             firstNumber = firstNumber - secondNumber;
             Console.WriteLine("After Swapping these Two Numbers : First number is =" + firstNumber + " and Second number is =" + secondNumber);
         }
+        public void Division()
+        {
+            Console.WriteLine("Enter the Dividend");
+            double dividend = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Divisor");
+            double divisor = Convert.ToDouble(Console.ReadLine());
+            double quotient = dividend / divisor;
+            double remainder = dividend % divisor;
+            Console.WriteLine("Quotient is : " + quotient);
+            Console.WriteLine("Remainder is : " + remainder);
+        }
     }
 }
+
 
 
 
