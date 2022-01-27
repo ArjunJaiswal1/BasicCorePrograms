0fe1cc660638d195ca53a4aaf160a24bca1e3dc4
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-01-27 23:36:05 +0530

    [Arjun] Add ; Added Switched

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -6,7 +6,7 @@ namespace BasicCoreProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. flipCoin \n 2. LeapYear\n 3. PowOfTwo\n 4. Primefactor\n 5. Harmonic\n 6. SwipingNumber\n 7. QuotientRemainder\n 8. Evenodd\n 9. Alphabet\n 10. Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. flipCoin \n 2. LeapYear\n 3. PowOfTwo\n 4. Primefactor\n 5. Harmonic\n 6. SwipingNumber\n 7. QuotientRemainder\n 8. Evenodd\n 9. Alphabet\n 10. LargestThreeNumbers\n 11. Exit");
 
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
@@ -60,8 +60,14 @@ namespace BasicCoreProblems
                         constantvowel.Alphabet();
                         break;
 
-
                     case 10:
+                        Basicprograms Among = new Basicprograms();
+                        Among.LargestNumber();
+                        break;
+
+
+
+                    case 11:
                         flag = false;
                         break;
                 }
